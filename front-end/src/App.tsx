85f85e66bff1257cb0f7e9dac81fea2a5d59b77f
Gustavo Tiburcio <gustavotiburcio23@gmail.com>
2022-11-16 17:54:59 -0300

    Estruturando front-end

---
+++ front-end/src/App.tsx
@@ -0,0 +1,12 @@
+import { GlobalStyles } from './styles/GlobalStyles';
+
+import { Header } from './components/Header';
+
+export function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <Header />
+    </>
+  );
+}
